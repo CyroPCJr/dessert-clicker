8a206eebb50f8dc223d7f63e55bdf985ac5a2923
Cyro Paulino da Costa Jr <cyropcjr@gmail.com>
2024-07-27 12:49:39 -0700

    Update: Update moving all funcions to MVVM design pattern. (#1)

---
+++ app/src/main/java/com/example/dessertclicker/model/DessertUIState.kt
@@ -0,0 +1,9 @@
+package com.example.dessertclicker.model
+
+data class DessertUIState(
+    val revenue: Int = 0,
+    val dessertSold: Int = 0,
+    val currentDessertIndex: Int = 0,
+    val currentDessertPrice: Int = 0,
+    val currentDessertImageId: Int = 0,
+)
